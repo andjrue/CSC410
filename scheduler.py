afa78cba7a642d7ab916c313105a681ca2677900
andjrue <drewsimcox12@gmail.com>
2024-05-22 12:42:13 -0400

    Mostly done, might need to wrap up a few optional items

--- scheduler.py
+++ scheduler.py
@@ -153,7 +153,17 @@ if options.solve == True:
     wait = {}
     remaining_time = {}  # Need a place to hold remaining time
     job_arrival_time = {}
+    completion_time = {} # Need a place to hold the completion time of a task
     thetime = 0.0
+    '''
+    time_increase = 2 <-- I thought this would be really cool to add. Unfortunately, the program explodes if 
+    the time isn't set to 1 at all times. I imagine it has something to do with some start times not being available
+    if we don't "check every second". 
+    
+    For example: If something has a start time of 2, but we increment time by 3, then the computer doesn't actually recognize 
+    the program should've started at the second "tick" and actually should have run for a second. I'll leave it at 1 for now,
+    it would be possible to add a check for that but it's Wednesday and I'm running low on time. 
+    '''
     jobcount = len(joblist)
 
     for job in joblist:
@@ -161,7 +171,7 @@ if options.solve == True:
       remaining_time[job_number] = job[1] # Maps to remaining_time
       wait[job_number] = 0.0
       turnaround[job_number] = 0.0
-      response[job_number] = -1
+      response[job_number] = -1 # Stole this from RR
       job_arrival_time[job_number] = job[2]
       # print(remaining_time)
       # print(wait)
@@ -169,40 +179,69 @@ if options.solve == True:
       # print(response)
       # All debugs
 
-      ### DO NOT FORGET TO CONVERT THE STRINGS BACK TO INTS ANDREW
 
     runlist = []
     for e in joblist:
       runlist.append(e)
-      print(runlist)
-    while jobcount > 0:
-      runlist = sorted(runlist, key=lambda x: x[2])  # Sorts the list by Start time
-      '''
-      The sort is really important. It makes the rest of the problem much easier, because now we know 
-      exaclty which order the jobs will be received. The rest of the problem is some simple math.  
-      '''
+      # print(runlist)
 
+    while jobcount > 0:
       available_jobs = []
       for job in joblist:
         job_number = job[0]
         if job_arrival_time[job_number] <= thetime and remaining_time[job_number] > 0:
           available_jobs.append(job)
-      print(available_jobs)
 
       if available_jobs:
-        available_jobs.sort(key=lambda x: remaining_time[x[0]])
+        available_jobs.sort(key=lambda x: remaining_time[x[0]]) # Sorts jobs by remaining time
+        # https://www.geeksforgeeks.org/python-sort-tuple-list-by-nth-element-of-tuple/
         current_job = available_jobs[0]
         job_number = current_job[0]
         runtime = remaining_time[job_number]
+        # print(f"Available jobs debug: {available_jobs}")
+
+        if response[job_number] == -1: # Stole this from RR as well
+          response[job_number] = thetime
+
+        print('  [ time %3d ] Run job %3d for %.2f secs' % (thetime, job_number, 1))
+        remaining_time[job_number] -= 1 # After it runs, we can remove the time taken from the remaining time
+        thetime += 1 # And increment total time by the time_increase variable
+
+        if remaining_time[job_number] == 0: # Once there is no more remaining time for a job...
+          completion_time[job_number] = thetime
+          turnaround[job_number] = thetime - job_arrival_time[job_number]
+          print(f"Job Number: {job_number} (COMPLETE)") # A nice debug to make sure each job is completed as expected
+
+          jobcount -= 1 # ...We can remove it from the total job count
+
+      else:
+        # I added this to keep track of time spent w/o an available job.
+        # I found it was pretty useful in some of the test cases I ran. The start time of the first task
+        # isn't always 0 and this allowed me to visualize that.
+        print('  [ time %3d ] No job run -> Time spent:  %.2f secs' % (thetime, 1))
+        thetime += 1 # Increment time by one if no available tasks
 
 
+    for job in joblist:
+      job_number = job[0]
+      wait[job_number] = turnaround[job_number] - job[1]
+      # print(job[1])
 
-      print(runlist)
+    print('\nFinal statistics:') # Stolen from RR
+    turnaroundSum = 0.0
+    waitSum = 0.0
+    responseSum = 0.0
+    for i in range(0, len(joblist)):
+      turnaroundSum += turnaround[i]
+      responseSum += response[i]
+      waitSum += wait[i]
+      print('  Job %3d -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f' % (i, response[i], turnaround[i], wait[i]))
+    count = len(joblist)
 
-      # for i in runlist:
+    print('\n  Average -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f\n' % (
+      responseSum / count, turnaroundSum / count, waitSum / count))
 
-    # jobnum = job[0]
-    # runtime = float(job[1])
+  # I believe the math is mathing correctly here. I've been through a bunch of test cases and haven't noticed anything unusual.
 
   # STCF ends here
 
